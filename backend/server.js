b06d3f721fa509b48f4ff47b19d97bca1681ae27
fedot Mosorkin <fmosorkin@gmail.com>
2024-03-25 23:49:52 +0900

    Trying to add cors

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,8 @@ const swaggerDocument = require("./swagger.json");
 const app = express();
 const port = 3054;
 
+app.use(cors());
 app.use(bodyParser.json());
-
-app.use(
-  cors({
-    origin: "http://localhost:3054/",
-    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-    credentials: true,
-  })
-);
 
 mongoose.connect("mongodb://localhost:3055/mydb", {
   useNewUrlParser: true,
