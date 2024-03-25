be839ee2cf8dc79b80efd89e2f42489b84eef146
fedot Mosorkin <fmosorkin@gmail.com>
2024-03-25 23:34:37 +0900

    Added cors require

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = requre("cors");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
