a3e7064e8274d8f91700566027ceabc93975cf99
fedot Mosorkin <fmosorkin@gmail.com>
2024-03-25 22:34:26 +0900

    Updated swagger.json, added Dockerfiles, added users schema

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,11 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
 const app = express();
-const port = 3000;
+const port = 3054;
 
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost:27017/", {
+mongoose.connect("mongodb://localhost:3055/mydb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -20,21 +20,26 @@ const postSchema = new mongoose.Schema({
   content: String,
 });
 
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
 const Post = mongoose.model("Post", postSchema);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const user = users.find(
-    (u) => u.username === username && u.password === password
-  );
+  const user = await User.findOne({ username, password });
 
   if (user) {
     const accessToken = jwt.sign({ username: user.username }, "secretKey");
     res.json({ accessToken });
   } else {
-    res.status(401).send("Username or password is incorrect");
+    res.status(401).send("Имя пользователя или пароль неверны");
   }
 });
 
@@ -43,23 +48,18 @@ app.get("/posts", authenticateToken, async (req, res) => {
   res.json(posts);
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
-  if (users.find((u) => u.username === username)) {
-    return res.status(400).send("User already exists");
+  const existingUser = await User.findOne({ username });
+
+  if (existingUser) {
+    return res.status(400).send("Пользователь уже существует");
   }
 
-  const newUser = { id: users.length + 1, username, password };
-  users.push(newUser);
+  const newUser = new User({ username, password });
+  await newUser.save();
   res.json(newUser);
-});
-
-app.post("/posts", authenticateToken, async (req, res) => {
-  const { title, content } = req.body;
-  const newPost = new Post({ title, content });
-  await newPost.save();
-  res.json(newPost);
 });
 
 function authenticateToken(req, res, next) {
@@ -72,7 +72,7 @@ function authenticateToken(req, res, next) {
 
   jwt.verify(token, "secretKey", (err, user) => {
     if (err) {
-      return res.sendStatus(403);
+      return res.sendStatus(401);
     }
     req.user = user;
     next();
